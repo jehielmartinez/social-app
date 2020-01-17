d6aa8436c6ad98225476113c37a36c18d69a4c67
Jehiel Martinez <jehielmartinez@Jehiels-MacBook-Pro.local>
2020-01-16 21:23:15 -0600

    initial structure

---
+++ api/index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const config = require('../config');
+const user = require('./components/user/network');
+
+const app = express();
+
+//Router
+app.use('/api/user', user);
+
+app.listen(config.api.port, () => {
+    console.log('API listening on port', config.api.port);
+});
