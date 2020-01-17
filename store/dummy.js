7fc0316ae60bf6db2ca1de15a66a648c460769ae
Jehiel Martinez <jehielmartinez@Jehiels-MacBook-Pro.local>
2020-01-16 21:41:26 -0600

    user routes

--- store/dummy.js
+++ store/dummy.js
@@ -1,20 +1,24 @@
 const db = {
     'user': [
-        {id: 1, name: 'John'}
+        {id: 1, name: 'John'},
+        {id: 2, name: 'Ron'}
     ]
 };
 
-function list(table) {
+async function list(table) {
     return db[table];
 }
-function get(table, id) {
-    let collection = list(table);
-    return collection.filter(item => item.id === id)[0] || null;
+
+async function get(table, id) {
+    let collection = await list(table);
+    return collection.filter(item => item.id == id)[0] || null;
 }
-function upsert(table, data) {
+
+async function upsert(table, data) {
     db[collection].push(data);
 }
-function remove(table, data) {
+
+async function remove(table, data) {
     return true
 }
 
